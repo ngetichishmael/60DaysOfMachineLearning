c874a8283b719f7a8ebafb2e7b6f30896fec0e2f
ish <78224577+ngetichishmael@users.noreply.github.com>
2024-07-15 21:27:03 +0300

    add: main function for entire dataset

---
+++ Week 4/KNN/main.py
@@ -0,0 +1,30 @@
+from outlook_entropy import outlook_results
+from temperature_entropy import temp_results
+from humidity_entropy import humidity_results
+from wind_entropy import wind_results
+from sunny_subset_entropy import sunny_subset_results
+from rain_subset_entropy import rain_subset_results
+
+print("Outlook Start")
+print(outlook_results)
+print("Outlook End")
+
+print("Temperature Start")
+print(temp_results)
+print("Temperature End")
+
+print("Humidity Start")
+print(humidity_results)
+print("Humidity End")
+
+print("Wind Start")
+print(wind_results)
+print("Wind End")
+
+print("Sunny Subset Start")
+print(sunny_subset_results)
+print("Sunny Subset End")
+
+print("Rain Subset Start")
+print(rain_subset_results)
+print("Rain Subset End")
